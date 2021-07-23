7c0907eb0228f5fc72512d1d512f60b2484e8f9e
Alexander Vasiev <cadistortion@gmail.com>
2021-07-23 14:20:09 +0300

    Added nodemon to backend container. Backend (express) now listening on port 80.

--- dev/frontend/src/App.js
+++ dev/frontend/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {
@@ -9,15 +10,42 @@ import {
   userParams
 } from "react-router-dom";
 
+function setApiAvailability(what) {
+  console.log(what);
+}
+
 function App() {
-  return (<Router>
-    <Switch>
-      <Route path='/'>
-        Public content.
-        Authorized only content!!!
-      </Route>
-    </Switch>
-  </Router>);
+
+  const [backendOnline, updateBackendOnline] = useState(false);
+  const [backendLastResponse, updateBackendLastResponse] = useState('');
+
+  const checkBackendOnline = () => {
+    fetch('/api')
+    .then((response) => {return console.log(response);})
+    .then((data) => {
+      updateBackendOnline(data != null);
+      updateBackendLastResponse(data);
+    });
+  };
+
+  return (
+    <Router>
+      <Switch>
+        <Route path='/'>
+          {
+            backendOnline ?
+              <h3>
+                Backend ONLINE! {backendLastResponse}
+              </h3>
+              :
+              <h3>
+                Backend offline... damn... {backendLastResponse}
+              </h3>
+          }
+          <button onClick={checkBackendOnline}>Check IS backend online</button>
+        </Route>
+      </Switch>
+    </Router>);
 }
 
 export default App;
